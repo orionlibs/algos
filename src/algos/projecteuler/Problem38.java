d3f9a12679bf983cf844a44a7e0c36ee032ae5ce
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2018-11-13 16:18:04 +0000

    1

---
+++ src/algos/projecteuler/Problem38.java
@@ -0,0 +1,18 @@
+package algos.projecteuler;
+
+//Pandigital multiples = https://projecteuler.net/problem=38
+public class Problem38
+{
+    public static void main(String[] args)
+    {
+        System.out.println(solve());
+    }
+
+
+    private static long solve()
+    {
+        
+        
+        return 0;
+    }
+}
