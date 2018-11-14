a10c3bfdde0030da56c10aa891d9216888604cce
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2018-11-14 15:43:52 +0000

    1

--- src/algos/projecteuler/Problem38.java
+++ src/algos/projecteuler/Problem38.java
@@ -1,5 +1,7 @@
 package algos.projecteuler;
 
+import java.math.BigInteger;
+
 //Pandigital multiples = https://projecteuler.net/problem=38
 public class Problem38
 {
@@ -11,8 +13,26 @@ public class Problem38
 
     private static long solve()
     {
+        long largestPandigital = 0;
         
+        for(long i = 1; i <= 99999; i++)
+        {
+            String digits = "";
+            
+            for(long j = 1; j <= 50; j++)
+            {
+                digits += BigInteger.valueOf(i).multiply(BigInteger.valueOf(j)).toString();
+                
+                if(digits.length() == 9 && digits.contains("1") && digits.contains("2") && digits.contains("3")
+                                && digits.contains("4") && digits.contains("5") && digits.contains("6")
+                                && digits.contains("7") && digits.contains("8") && digits.contains("9")
+                                && i > largestPandigital)
+                {
+                    largestPandigital = Long.parseLong(digits);
+                }
+            }
+        }
         
-        return 0;
+        return largestPandigital;
     }
 }
